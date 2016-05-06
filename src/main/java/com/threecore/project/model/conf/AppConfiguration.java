886e7fb1ec1e011b518e1b1792382c6d81e17647
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-05-06 02:19:04 +0200

    feedback timeout restore original

--- src/main/java/com/threecore/project/model/conf/AppConfiguration.java
+++ src/main/java/com/threecore/project/model/conf/AppConfiguration.java
@@ -7,33 +7,33 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import com.threecore.project.model.type.Stringable;
 
 public final class AppConfiguration implements Stringable {
-	
+
 	private String friendships;
 	private String posts;
 	private String comments;
 	private String likes;
 	private int k;
 	private int d;
-	
+
 	private String outdir;
-	
+
 	private boolean autoWatermark;
 	private long watermarkDelay;
-	
+
 	private long iterationTimeout;
 	private long bufferTimeout;
-	
+
 	private int parallelism;
-	
+
 	private double memoryThreshold;
-	
+
 	private String redisHostname;
 	private int redisPort;
-	
+
 	private int sinkBufferSize;
-	
-	private boolean debug;	
-	
+
+	private boolean debug;
+
 	public AppConfiguration() {
 		this.friendships = null;
 		this.posts = null;
@@ -44,48 +44,48 @@ public final class AppConfiguration implements Stringable {
 		this.outdir = null;
 		this.autoWatermark = false;
 		this.watermarkDelay = 1000;
-		this.iterationTimeout = 30000;
+		this.iterationTimeout = 180000; //180000
 		this.bufferTimeout = -1;
 		this.parallelism = 16;
 		this.memoryThreshold = 0.75;
 		this.redisHostname = "localhost";
 		this.redisPort = 6379;
 		this.sinkBufferSize = 24576;
-		this.debug = false;		
+		this.debug = false;
 	}
-	
+
 	public String getFriendships() {
 		return this.friendships;
 	}
-	
+
 	public void setFriendships(String friendships) {
 		this.friendships = friendships;
 	}
-	
+
 	public String getPosts() {
 		return this.posts;
 	}
-	
+
 	public void setPosts(String posts) {
 		this.posts = posts;
 	}
-	
+
 	public String getComments() {
 		return this.comments;
 	}
-	
+
 	public void setComments(String comments) {
 		this.comments = comments;
 	}
-	
+
 	public String getLikes() {
 		return this.likes;
 	}
-	
+
 	public void setLikes(String likes) {
 		this.likes = likes;
 	}
-	
+
 	public int getK() {
 		return this.k;
 	}
@@ -101,7 +101,7 @@ public final class AppConfiguration implements Stringable {
 	public void setD(int d) {
 		this.d = d;
 	}
-	
+
 	public String getOutdir() {
 		return this.outdir;
 	}
@@ -109,39 +109,39 @@ public final class AppConfiguration implements Stringable {
 	public void setOutdir(String outdir) {
 		this.outdir = outdir;
 	}
-	
+
 	public long getWatermarkDelay() {
 		return this.watermarkDelay;
 	}
-	
+
 	public void setWatermarkDelay(long watermarkDelay) {
 		this.watermarkDelay = watermarkDelay;
 	}
-	
+
 	public boolean getAutoWatermark() {
 		return this.autoWatermark;
 	}
-	
+
 	public void setAutoWatermark(boolean autoWatermark) {
 		this.autoWatermark = autoWatermark;
 	}
-	
+
 	public long getIterationTimeout() {
 		return this.iterationTimeout;
 	}
-	
+
 	public void setIterationTimeout(long iterationTimeout) {
 		this.iterationTimeout = iterationTimeout;
 	}
-	
+
 	public long getBufferTimeout() {
 		return this.bufferTimeout;
 	}
-	
+
 	public void setBufferTimeout(long bufferTimeout) {
 		this.bufferTimeout = bufferTimeout;
 	}
-	
+
 	public int getParallelism() {
 		return this.parallelism;
 	}
@@ -157,27 +157,27 @@ public final class AppConfiguration implements Stringable {
 	public void setMemoryThreshold(double memoryThreshold) {
 		this.memoryThreshold = memoryThreshold;
 	}
-	
+
 	public String getRedisHostname() {
 		return this.redisHostname;
 	}
-	
+
 	public void setRedisHostname(final String redisHostname) {
 		this.redisHostname = redisHostname;
 	}
-	
+
 	public int getRedisPort() {
 		return this.redisPort;
 	}
-	
+
 	public void setRedisPort(final int redisPort) {
 		this.redisPort = redisPort;
 	}
-	
+
 	public int getSinkBufferSize() {
 		return this.sinkBufferSize;
 	}
-	
+
 	public void setSinkBufferSize(final int buffsize) {
 		this.sinkBufferSize = buffsize;
 	}
@@ -185,29 +185,29 @@ public final class AppConfiguration implements Stringable {
 	public boolean getDebug() {
 		return this.debug;
 	}
-	
+
 	public void setDebug(boolean debug) {
 		this.debug = debug;
 	}
-	
+
 	public String getSinkPath(final String jobname) {
 		return String.format("%s/%s/out.txt", this.getOutdir(), jobname);
-	}	
-	
+	}
+
 	public String getPerformancePath(final String jobname) {
 		return String.format("%s/%s/performance.txt", this.getOutdir(), jobname);
 	}
-	
+
 	public String getLogPath(final String jobname) {
 		return String.format("%s/%s/log.txt", this.getOutdir(), jobname);
 	}
-	
+
 	// DEBUG
-	
+
 	public String getSinkPath(final String jobname, final String out) {
 		return String.format("%s/%s/%s.txt", this.getOutdir(), jobname, out);
-	}	
-	
+	}
+
 	@Override
 	public String toString() {
 		return new ToStringBuilder(this)
@@ -248,6 +248,6 @@ public final class AppConfiguration implements Stringable {
 				.add("sinkBufferSize: " + this.getSinkBufferSize())
 				.add("debug: " + this.getDebug())
 				.toString();
-	}	
+	}
 
 }
